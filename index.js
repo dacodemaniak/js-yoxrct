b68d35399be4eea105ff2f65ddd1134fe68de2b5
dacodemaniak <jean-luc.aubert@web-projet.com>
2022-07-02 16:10:49 +0200

    Using Intersect Observer

--- index.js
+++ index.js
@@ -4,3 +4,34 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// Check if a div goes in inner vertical
+const observer = new IntersectionObserver(
+  (entries) => {
+    if (entries[0]['isIntersecting'] === true) {
+      if (entries[0]['intersectionRatio'] === 1)
+        document.querySelector('#message').textContent =
+          'Target is fully visible in screen : ' +
+          entries[0].target.getAttribute('data-value');
+      else if (entries[0]['intersectionRatio'] > 0.5)
+        document.querySelector('#message').textContent =
+          'More than 50% of target is visible in screen : ' +
+          entries[0].target.getAttribute('data-value');
+      else
+        document.querySelector('#message').textContent =
+          'Less than 50% of target is visible in screen : no value required';
+    } else {
+      document.querySelector('#message').textContent =
+        'Target is not visible in screen : no value required';
+    }
+  },
+  {
+    threshold: [0, 0.5, 1],
+    root: document.querySelector('#vertical-scroll-container'),
+  }
+);
+
+const numbers = document.querySelectorAll('.scroll-item');
+numbers.forEach((number) => {
+  observer.observe(number);
+});
