c3d4dad6bf8a8f6137749e43cb1f61e638112e07
dacodemaniak <jean-luc.aubert@web-projet.com>
2022-07-03 09:06:15 +0200

    Move to flex

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const randomNumber = 3;
 
 const targetElementPos = document.querySelector('[data-value="3"').offsetTop;
 document.querySelector('#vertical-scroll-container').scrollTop =
-  targetElementPos;
+  targetElementPos - 16;
 
 // Check if a div goes in inner vertical
 const observer = new IntersectionObserver(
@@ -20,20 +20,10 @@ const observer = new IntersectionObserver(
         document.querySelector('#message').textContent =
           'Target is fully visible in screen : ' +
           entries[0].target.getAttribute('data-value');
-      else if (entries[0]['intersectionRatio'] > 0.5)
-        document.querySelector('#message').textContent =
-          'More than 50% of target is visible in screen : ' +
-          entries[0].target.getAttribute('data-value');
-      else
-        document.querySelector('#message').textContent =
-          'Less than 50% of target is visible in screen : no value required';
-    } else {
-      document.querySelector('#message').textContent =
-        'Target is not visible in screen : no value required';
     }
   },
   {
-    threshold: [0, 0.5, 1],
+    threshold: [1],
     root: document.querySelector('#vertical-scroll-container'),
   }
 );
